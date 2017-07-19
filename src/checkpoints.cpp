e920ee1e36083130486585f1cc8e308a95437ae3
Signatum <signatum@protonmail.com>
2017-07-19 14:50:36 +0500

    update checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -31,6 +31,7 @@ namespace Checkpoints
         (  500,     uint256("0xc842ceeb227fafaeb90c9b3c820da8f2c1d7debd77ab9f879bf975add00c48ab") )
         (  750,     uint256("0xbb5b5f8f4052dde501d2aad794c49347222a7f900b1097607e0492a4d11bd287") )
         (  1250,     uint256("0x6dc761812a65430115dd49a6da3ffc1e4f66bdb089a30fe43a12c7bd60f5ed0d") )
+        (  1810,     uint256("0xb524c9c69bad926472b04138c2d3c934045d4d0be9199a042ecf794a82e1799f") )
     ;
 
     // TestNet has no checkpoints
